8f2463bff1b98971927dbff554c359a408d82235
Ben Robinson <robinson.dri@gmail.com>
2021-01-10 19:11:06 -0800

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -12,7 +12,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 require("./routes/api_routes")(app);
 require("./routes/html_routes")(app);
